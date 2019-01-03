33d56041a0728775d48cf61613a3a02e9c66e0f6
Jon Huynh <jhsoccer123@gmail.com>
2019-01-03 16:04:13 -0500

    Initial commit

---
+++ xyz-machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Jon Huynh on 1/3/19.
+//  Copyright © 2019 Jon Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
